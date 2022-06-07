7f4a8293db0d25c7bba96a5e08af03e22066bf56
carreuky <carreuky@gmail.com>
2022-06-07 15:59:56 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(plan='roller-skate') {
+ return `This Saturday, I want to ${plan}!`
+}
+saturdayFun()
+
+function mondayWork(plan='go to the office') {
+    return `This Monday, I will ${plan}.`
+   }
+   mondayWork('This Monday, I will work from home.')
+function wrapAdjective(adjective){
+   const specialChar=function(char){
+       return `You are ${adjective}${char}${adjective}!`
+   }
+   return specialChar
+}
+wrapAdjective('*')
